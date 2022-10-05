ec1f31a1df933d09cb472c61e43586a8ae00fa47
Israel A. Oteniola <success.ote86@gmail.com>
2022-10-05 12:22:50 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    args = sys.argv
-    args.pop(0)
-    argc = len(args)
-    line1 = "{:d} argument".format(argc)
-    if argc == 0:
-        line1 += "s."
-    elif argc == 1:
-        line1 += ":"
-    else:
-        line1 += "s:"
-    print(line1)
-    for i, arg in enumerate(args):
-        print("{:d}: {:s}".format(i+1, arg))
+if __name__ == "__main__":
+    import sys
+    num_arg = len(sys.argv) - 1
+    print("{:d} {:s}{:s}".format(
+        num_arg,
+        "argument" if num_arg == 1 else "arguments",
+        "." if num_arg == 0 else ":"))
+    for i, arg in enumerate(sys.argv[1:]):
+        print("{:d}: {:s}".format(i + 1, arg))
